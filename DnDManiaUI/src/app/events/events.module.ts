f08dcc50bfeef23c8c8e2ea6db8dce11b0d176a0
Jae-Hwang <jae.hwang@revature.com>
2019-12-03 16:43:31 -0500

    Commit before pulling

--- DnDManiaUI/src/app/events/events.module.ts
+++ DnDManiaUI/src/app/events/events.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventsContainerComponent } from './components/events-container/events-container.component';
-import { EventsRoutingModule } from './events-routing.module';
 import { EventComponent } from './components/event/event.component';
 import { EventService } from './services/event.service';
 import { FormsModule } from '@angular/forms';
+import { EventsRoutingModule } from './events-routing.module';
 
 
 
