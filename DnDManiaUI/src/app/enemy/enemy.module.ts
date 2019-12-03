87234235db70e3906df4160333fc04a53438adf3
David-Quillen9826 <david.quillen@revature.net>
2019-12-03 09:17:01 -0500

    Pushing Model

--- DnDManiaUI/src/app/enemy/enemy.module.ts
+++ DnDManiaUI/src/app/enemy/enemy.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MonsterComponent } from './monster/monster.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MonsterComponent],
   imports: [
     CommonModule
   ]
---
+++ DnDManiaUI/src/app/enemy/models/enemy.model.ts
@@ -0,0 +1,10 @@
+export class Enemy {
+    constructor(
+        public id = 0,
+        public name = '',
+        public challenge = 0,
+        public healthPoints = 0,
+        public armorClass = 0,
+        public attack = 0,
+    ) {}
+}
