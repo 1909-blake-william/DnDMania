b7c1f57bb18167be08f40eb2d48b6739931f2cf8
David-Quillen9826 <david.quillen@revature.net>
2019-12-07 13:51:48 -0500

    removed monster component from routing module

--- DnDManiaUI/src/app/app-routing.module.ts
+++ DnDManiaUI/src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { TestButtonComponent } from './components/test-button/test-button.component';
 import { ViewComponent } from './view/view/view.component';
 import { CharacterComponent } from './components/character/character.component';
-import { MonsterComponent } from './enemy/monster/monster.component'
 
 const routes: Routes = [
   {
@@ -29,11 +28,6 @@ const routes: Routes = [
     loadChildren: () => import('./enemy/enemy.module').then(mod => mod.EnemyModule)
   },
   {
-    path: 'aa',
-    pathMatch: 'full',
-    redirectTo: '/not-found'
-  },
-  {
     path: 'view',
     component: ViewComponent
   },
@@ -42,9 +36,15 @@ const routes: Routes = [
     component: CharacterComponent
   },
   {
-    path: 'monster',
-    component: MonsterComponent
-  }
+    path: 'aa',
+    pathMatch: 'full',
+    redirectTo: '/not-found'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/not-found'
+  },
 ];
 
 @NgModule({
