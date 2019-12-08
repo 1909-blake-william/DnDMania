a6e5675c35ce0766abe211e22da3e09f797c12e3
David-Quillen9826 <david.quillen@revature.net>
2019-12-07 20:01:31 -0500

    finished monster model, created monster dao

--- DNDManiaServer/src/main/java/com/revature/daos/MonsterDaoSql.java
+++ DNDManiaServer/src/main/java/com/revature/daos/MonsterDaoSql.java
@@ -1,11 +1,32 @@
 package com.revature.daos;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.revature.models.Monster;
 
 public class MonsterDaoSql implements MonsterDao {
+	
+	private static final String FIND_ALL = "SELECT  FROM mon_group ";
 
+	
+	Monster extract(ResultSet rs) throws SQLException {
+		return new Monster(
+				rs.getInt("monster_id"),
+				rs.getString("name"),
+				rs.getInt("challenge"),
+				rs.getInt("healthpoints"),
+				rs.getInt("armorclass"),
+				rs.getInt("attack"),
+				rs.getInt("dmg"),
+				rs.getInt("dmgmod"),
+				rs.getInt("initiative"),
+				rs.getInt("initiativemod"),
+				rs.getInt("entity_type")
+			);
+	}
+	
 	public List<Monster> findAll() {
 		// TODO Auto-generated method stub
 		return null;
