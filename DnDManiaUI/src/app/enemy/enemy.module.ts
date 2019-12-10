d11960bc03f10e2803387e895cb062c14ae4363b
David-Quillen9826 <david.quillen@revature.net>
2019-12-09 21:25:31 -0500

    fixed get group function

--- DnDManiaUI/src/app/enemy/enemy.module.ts
+++ DnDManiaUI/src/app/enemy/enemy.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MonsterComponent } from './monster/monster.component';
 import { MonsterCardComponent } from './monster-card/monster-card.component';
 import { EnemyRoutingModule } from './enemy-routing.module';
-
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [MonsterComponent, MonsterCardComponent],
   imports: [
     CommonModule,
-    EnemyRoutingModule
+    EnemyRoutingModule,
+    FormsModule
   ]
 })
 export class EnemyModule { }
--- DnDManiaUI/src/app/enemy/monster/monster.component.ts
+++ DnDManiaUI/src/app/enemy/monster/monster.component.ts
@@ -8,20 +8,34 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./monster.component.css']
 })
 export class MonsterComponent implements OnInit {
+  groupnumber: number = 0;
   oops: Enemy[] = [];
-  hi = [new Enemy(1,'f',1,1,1,1,1,1,1,1), new Enemy(2,'g',2,2,2,2,2,2,2,2), new Enemy(3,'h',3,3,3,3,3,3,3,3)];
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    
-    this.httpClient.get<Enemy[]>('http://localhost:8080/project2/monsters', {
-    })
-      .subscribe(data => {
-        console.log(data);
-        this.oops = data;
-      }, err => {
-        console.log(err);
-      });
+
+
   }
 
+  getGroup() {
+    if (this.groupnumber === 0) {
+      this.httpClient.get<Enemy[]>('http://localhost:8080/project2/monsters', {
+      })
+        .subscribe(data => {
+          console.log(data);
+          this.oops = data;
+        }, err => {
+          console.log(err);
+        });
+    } else if (this.groupnumber <= 9 && this.groupnumber > 0) {
+      this.httpClient.get<Enemy[]>(`http://localhost:8080/project2/monsters?groupnumber=${this.groupnumber}`)
+        .subscribe(data => {
+          this.oops = data;
+        }, err => {
+          console.log(err);
+        });
+    } else {
+      console.log("invalid number")
+    }
+  }
 }
